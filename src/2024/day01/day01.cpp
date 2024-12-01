ea213fba921c76cf1e7e24c01cfad0743ca6b78c
Eli Byrd <eli@thebyrdnest.net>
2024-12-01 00:06:01 -0600

    Solved 2024 Day 1

--- src/2024/day01/day01.cpp
+++ src/2024/day01/day01.cpp
@@ -10,15 +10,61 @@ Day01::Day01(const std::vector<std::string>& inputData) : Day(inputData) {}
 
 // Initialize resources specific to Day01
 void Day01::Initialize() {
-    
+    // No specific resources to initialize
 }
 
 // Solve Part 1
 std::string Day01::SolvePart1() {
-    return "";
+    // Initialize vectors and sum
+    std::vector<long> left;
+    std::vector<long> right;
+    long long sum = 0;
+
+    // Split the input into two vectors of longs
+    for (size_t i = 0; i < Input.size(); i++) {
+        left.push_back(std::stol(Input[i].substr(0, Input[i].find(" "))));
+        right.push_back(std::stol(Input[i].substr(Input[i].find(" ") + 3)));
+    }
+
+    // Sort both vectors
+    std::sort(left.begin(), left.end());
+    std::sort(right.begin(), right.end());
+
+    // Loop through both vectors and add the absolute difference of each element
+    for (size_t i = 0; i < left.size(); i++) {
+        if (left[i] > right[i]) {
+            sum += left[i] - right[i];
+        } else {
+            sum += right[i] - left[i];
+        }
+    }
+
+    // Return the sum as a string
+    return std::to_string(sum);
 }
 
 // Solve Part 2
 std::string Day01::SolvePart2() {
-    return "";
+    // Initialize vectors and sum
+    std::vector<long> left;
+    std::vector<long> right;
+    long long sum = 0;
+
+    // Split the input into two vectors of longs
+    for (size_t i = 0; i < Input.size(); i++) {
+        left.push_back(std::stol(Input[i].substr(0, Input[i].find(" "))));
+        right.push_back(std::stol(Input[i].substr(Input[i].find(" ") + 3)));
+    }
+
+    // Sort both vectors
+    std::sort(left.begin(), left.end());
+    std::sort(right.begin(), right.end());
+
+    // Loop through left vector and multiply by the number of occurences in right vector
+    for (size_t i = 0; i < left.size(); i++) {
+        sum += left[i] * std::count(right.begin(), right.end(), left[i]);
+    }
+
+    // Return the sum as a string
+    return std::to_string(sum);
 }
--- src/2024/day01/test.cpp
+++ src/2024/day01/test.cpp
@@ -1,12 +1,28 @@
 #include "day01.h"
 #include <gtest/gtest.h>
 
-TEST(Day01, SolvePart1) {
-    EXPECT_EQ("9", "10");
+// The fixture for testing AdventOfCode 2024, day 1, part 1, example 1.
+TEST(Day01, SolvePart1_ExampleInput1) {
+    Day01 day01_all("2024-01e1.txt");
+    EXPECT_EQ(day01_all.SolvePart1(), "11");
 }
 
-TEST(Day01, SolvePart2) {
-    EXPECT_EQ("10", "10");
+// The fixture for testing AdventOfCode 2024, day 1, part 2, example 1.
+TEST(Day01, SolvePart2_ExampleInput1) {
+    Day01 day01_all("2024-01e1.txt");
+    EXPECT_EQ(day01_all.SolvePart2(), "31");
+}
+
+// The fixture for testing AdventOfCode 2024, day 1, part 1, puzzle input.
+TEST(Day01, SolvePart1_PuzzleInput) {
+    Day01 day01("2024-01.txt");
+    EXPECT_EQ(day01.SolvePart1(), "3714264");
+}
+
+// The fixture for testing AdventOfCode 2024, day 1, part 2, puzzle input.
+TEST(Day01, SolvePart2_PuzzleInput) {
+    Day01 day01("2024-01.txt");
+    EXPECT_EQ(day01.SolvePart2(), "18805872");
 }
 
 int main(int argc, char** argv) {
