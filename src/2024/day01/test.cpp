8bf990caf989e8e5480061ceb2790babb44b956e
Eli Byrd <eli@thebyrdnest.net>
2024-11-30 23:00:06 -0600

    Make test to make sure it fails

--- src/2024/day01/test.cpp
+++ src/2024/day01/test.cpp
@@ -2,11 +2,11 @@
 #include <gtest/gtest.h>
 
 TEST(Day01, SolvePart1) {
-    EXPECT_EQ("9", "10")
+    EXPECT_EQ("9", "10");
 }
 
 TEST(Day01, SolvePart2) {
-    EXPECT_EQ("10", "10")
+    EXPECT_EQ("10", "10");
 }
 
 int main(int argc, char** argv) {
