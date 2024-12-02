90e6cfb3b5f815dedcfdce585c3666fce69f378a
Eli Byrd <Eli@thebyrdnest.net>
2024-12-01 21:14:33 -0600

    A few updates

--- .template/.day/test.cpp
+++ .template/.day/test.cpp
@@ -1,12 +1,28 @@
 #include "day##.h"
 #include <gtest/gtest.h>
 
-TEST(Day##, SolvePart1) {
-    
+// The fixture for testing AdventOfCode &&&&, day ##, part 1, example 1.
+TEST(Day##, SolvePart1_ExampleInput1) {
+    Day## day##("&&&&-##e1.txt");
+    EXPECT_EQ(day##.SolvePart1(), "<Answer>");
 }
 
-TEST(Day##, SolvePart2) {
-    
+// The fixture for testing AdventOfCode &&&&, day ##, part 2, example 1.
+TEST(Day##, SolvePart2_ExampleInput1) {
+    Day## day##("&&&&-##e1.txt");
+    EXPECT_EQ(day##.SolvePart2(), "<Answer>");
+}
+
+// The fixture for testing AdventOfCode &&&&, day ##, part 1, puzzle input.
+TEST(Day##, SolvePart1_PuzzleInput) {
+    Day## day##("&&&&-##.txt");
+    EXPECT_EQ(day##.SolvePart1(), "<Answer>");
+}
+
+// The fixture for testing AdventOfCode &&&&, day ##, part 2, puzzle input.
+TEST(Day##, SolvePart2_PuzzleInput) {
+    Day## day##("&&&&-##.txt");
+    EXPECT_EQ(day##.SolvePart2(), "<Answer>");
 }
 
 int main(int argc, char** argv) {
